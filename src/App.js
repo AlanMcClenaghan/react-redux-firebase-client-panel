d94b06060b5bf16d75804f4850fe5170173fea36
AlanMcClenaghan <alandmcclenaghan@icloud.com>
2019-05-10 22:36:27 +0100

    Dashboard, Sidebar & Client Componets

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import AppNavbar from "../src/components/layout/AppNavbar";
+import Dashboard from "../src/components/layout/Dashboard";
 
 import "./App.css";
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <AppNavbar />
-        <div className="container">
-          <h1>Hello</h1>
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <AppNavbar />
+          <div className="container">
+            <switch>
+              <Route exact path="/" component={Dashboard} />
+            </switch>
+          </div>
         </div>
-      </div>
-    </Router>
-  );
+      </Router>
+    );
+  }
 }
 
 export default App;
