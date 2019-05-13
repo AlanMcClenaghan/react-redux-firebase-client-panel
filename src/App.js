a802717301c08d8f84dcd620391d2c89d7ae89a0
AlanMcClenaghan <alandmcclenaghan@icloud.com>
2019-05-13 18:45:58 +0100

    Edit Client in Firestore.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import store from "./store";
 import AppNavbar from "../src/components/layout/AppNavbar";
 import Dashboard from "../src/components/layout/Dashboard";
 import AddClient from "../src/components/clients/AddClient";
+import EditClient from "../src/components/clients/EditClient";
 import ClientDetails from "../src/components/clients/ClientDetails";
 
 import "./App.css";
@@ -22,6 +23,7 @@ class App extends Component {
               <Switch>
                 <Route exact path="/" component={Dashboard} />
                 <Route exact path="/client/add" component={AddClient} />
+                <Route exact path="/client/edit/:id" component={EditClient} />
                 <Route exact path="/client/:id" component={ClientDetails} />
               </Switch>
             </div>
