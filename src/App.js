502b0c4a4e0c924829867351b9e38add052df1b1
AlanMcClenaghan <alandmcclenaghan@icloud.com>
2019-05-14 22:11:46 +0100

    Login Functionality

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import store from "./store";
 
 import AppNavbar from "../src/components/layout/AppNavbar";
 import Dashboard from "../src/components/layout/Dashboard";
+import Login from "../src/components/auth/Login";
 import AddClient from "../src/components/clients/AddClient";
 import EditClient from "../src/components/clients/EditClient";
 import ClientDetails from "../src/components/clients/ClientDetails";
@@ -22,6 +23,7 @@ class App extends Component {
             <div className="container">
               <Switch>
                 <Route exact path="/" component={Dashboard} />
+                <Route exact path="/login" component={Login} />
                 <Route exact path="/client/add" component={AddClient} />
                 <Route exact path="/client/edit/:id" component={EditClient} />
                 <Route exact path="/client/:id" component={ClientDetails} />
